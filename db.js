d6961c0efc8acabd97bc591f8867d48ece6c6c7a
Aleksey Kazantsev <develop.kaa@gmail.com>
2016-06-14 11:09:45 +0300

    Add postgres fo production and config file support

--- db.js
+++ db.js
@@ -3,15 +3,16 @@ const path = require('path');
 const Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/config.json')[env];
+var sequelize;
 
-// console.log(config);
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (env === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 var db = {};
+
 db.todo = sequelize.import(path.join(__dirname, '/models/todo.js'));
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
